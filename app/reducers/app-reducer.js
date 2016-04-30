32afa62b0e40713be0f71d78c141d21ab955aee6
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2016-04-30 00:23:21 -0300

    add orchids reducer to app reducer

--- app/reducers/app-reducer.js
+++ app/reducers/app-reducer.js
@@ -5,9 +5,11 @@ import { combineReducers } from 'redux';
 import users from './users-reducer';
 import nurseries from './nurseries-reducer';
 import forms from './forms-reducer';
+import orchids from './orchids-reducer';
 
 export default combineReducers({
     users,
     nurseries,
     forms,
+    orchids,
 });
