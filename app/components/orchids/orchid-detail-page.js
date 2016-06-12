eeb812fbe9617428513d9cefb4f63c2d52995b75
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2016-06-12 14:05:36 -0300

    create orchids detail page

---
+++ app/components/orchids/orchid-detail-page.js
@@ -0,0 +1,82 @@
+'use strict';
+
+import React from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+
+import { merge, renderJSONStringToHTML } from '../../helpers/helpers';
+import { fetchOrchid, receiveOrchidSuccess } from '../../actions/orchids-actions';
+
+import Container from '../common/container';
+import LoadableContent from '../common/loadable-content';
+import Header from '../common/header';
+
+export default class OrchidDetailPage extends React.Component {
+    static contextTypes = {
+        store: React.PropTypes.object,
+    }
+
+    render = () => {
+        const { isLoading, data } = this.props.orchids;
+
+        return (
+            <div>
+                <LoadableContent isLoading={isLoading}>
+                    <div className="orchid-detail-image" style={{ backgroundImage: `url('${data.orchid.image || ''}')` }}>
+                        <div className="orchid-detail-image-top">
+                            <Container>
+                                <h1>{data.orchid.name || data.orchid.scientificName}</h1>
+                            </Container>
+                        </div>
+
+                        <div className="orchid-detail-image-bottom">
+                            <Container>
+                                <h5>
+                                    <strong>Nome Científico:</strong>{' '}
+                                    {data.orchid.scientificName}
+                                </h5>
+                                <h5>
+                                    <strong>Origem:</strong>{' '}
+                                    {data.orchid.origin}
+                                </h5>
+                            </Container>
+                        </div>
+                    </div>
+
+                    <Container spaced={true}>
+                        <button className="btn btn-primary dropdown-toggle">Adicionar</button>
+
+                        <hr/>
+
+                        <h2>Descrição:</h2>
+                        <div>{renderJSONStringToHTML(data.orchid.description)}</div>
+
+                        <h2>Instruções:</h2>
+                        <div>{renderJSONStringToHTML(data.orchid.instructions)}</div>
+                    </Container>
+                </LoadableContent>
+            </div>
+        );
+    }
+
+    componentDidMount = () => {
+        const { orchids } = this.props.orchids.data;
+        const { hash } = this.props.params;
+        const orchidLocalIndex = _.findIndex(orchids, { hash });
+        let orchid;
+
+        if (orchids && orchidLocalIndex >= 0) {
+            orchid = orchids[orchidLocalIndex];
+
+            this.context.store.dispatch(receiveOrchidSuccess(orchid));
+        } else {
+            this.context.store.dispatch(fetchOrchid(hash));
+        }
+    }
+}
+
+const mapStateToProps = (state) => {
+    return merge(state, {});
+}
+
+export const OrchidDetailContainer = connect(mapStateToProps)(OrchidDetailPage);
